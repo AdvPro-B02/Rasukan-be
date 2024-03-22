280184ce4c184a82784ddc1e0a9b7e0986a0350f
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2024-03-22 16:57:33 +0900

    initial commit

---
+++ src/main/java/advpro/b2/rasukan/RasukanApplication.java
@@ -0,0 +1,13 @@
+package advpro.b2.rasukan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RasukanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RasukanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/advpro/b2/rasukan/RasukanApplicationTests.java
@@ -0,0 +1,13 @@
+package advpro.b2.rasukan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RasukanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
