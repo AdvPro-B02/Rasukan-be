29d8fdae4b47914fe652b9f85d4ce4260c960cb0
Adrial <adrial2_nathanael@yahoo.com>
2024-04-17 16:56:34 +0700

    Change main app config

--- src/main/java/advpro/b2/rasukanbuysell/RasukanBuySellApplication.java
+++ src/main/java/advpro/b2/rasukanbuysell/RasukanBuySellApplication.java
@@ -2,8 +2,9 @@ package advpro.b2.rasukanbuysell;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication (exclude = {DataSourceAutoConfiguration.class})
 public class RasukanBuySellApplication {
 
     public static void main(String[] args) {
