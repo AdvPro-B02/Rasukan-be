6336bfd4f1844ee0f0b6be7e281d8c143feb7132
Adrial <adrial2_nathanael@yahoo.com>
2024-05-15 19:47:25 +0700

    [REFACTOR] fix Listing service

--- src/main/java/advpro/b2/rasukanbuysell/service/ListingServiceImpl.java
+++ src/main/java/advpro/b2/rasukanbuysell/service/ListingServiceImpl.java
@@ -2,14 +2,21 @@ package advpro.b2.rasukanbuysell.service;
 
 import java.util.*;
 
+import advpro.b2.rasukanbuysell.model.Builder.ListingBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.support.NullValue;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import advpro.b2.rasukanbuysell.model.Listing;
 //import advpro.b2.rasukanbuysell.model.User;
 import advpro.b2.rasukanbuysell.repository.ListingRepository;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class ListingServiceImpl implements ListingService{
@@ -17,19 +24,36 @@ public class ListingServiceImpl implements ListingService{
     @Autowired
     private ListingRepository listingRepository;
 
+    @Autowired
+    private ListingBuilder listingBuilder;
+
+//    private String authUrl = "http://localhost:8080/";
+
+    // TODO: buat test masih perlu bikin mock but restTemplate
+    private RestTemplate restTemplate;
+
     @Override
-    public Listing createListing(Listing listing){
-        Listing newListing = new Listing(listing.getName(), listing.getStock(), listing.getPrice(), listing.getSeller());
+    public Listing createListing(Listing listing, String token){
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", token);
+        HttpEntity<String> entity = new HttpEntity<>("body", headers);
+
+        // TODO: uncomment habis ada authUrl
+//        ResponseEntity<String > owner = restTemplate.exchange(authUrl+"user/get-username", HttpMethod.GET,entity ,String.class);
+
+
         try {
-            return listingRepository.save(listing);
-        } catch (DataIntegrityViolationException e) {
+            Listing newListing = new Listing(listing.getName(), listing.getStock(), listing.getPrice(), listing.getSeller());
+//            Listing newListing = new Listing(listing.getName(), listing.getStock(), listing.getPrice(), UUID.fromString(owner.getBody()));
+            return listingRepository.save(newListing);
+        } catch (Exception e) {
             return null;
         }
     }
 
     @Override
     public Optional<Listing> getListing(String listingId) {
-        return listingRepository.findById(UUID.fromString(listingId));
+        return listingRepository.findById(listingId);
     }
 
 
@@ -39,8 +63,8 @@ public class ListingServiceImpl implements ListingService{
 
         if (existingListing != null) {
             // dummy data
-            existingListing.setName("updatedName");
-            existingListing.setPrice(6969);
+            existingListing.setName(listing.getName());
+            existingListing.setPrice(listing.getPrice());
             // Set other properties as needed
 
             return listingRepository.save(existingListing);
@@ -51,7 +75,7 @@ public class ListingServiceImpl implements ListingService{
 
     @Override
     public void deleteListing(String listingId) {
-        listingRepository.deleteById(UUID.fromString(listingId));
+        listingRepository.deleteById(listingId);
     }
 
     @Override
@@ -61,6 +85,26 @@ public class ListingServiceImpl implements ListingService{
 //        List<Listing> allListings = new ArrayList<>();
 //        listingIterator.forEachRemaining(allListings::add);
 //        return allListings;
+    }
+
+    @Override
+    public Listing buildListing(Listing listing, String token) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", token);
+        HttpEntity<String> entity = new HttpEntity<>("body", headers);
+
+        // TODO: uncomment habis ada authUrl
+//        ResponseEntity<String > owner = restTemplate.exchange(authUrl+"user/get-username", HttpMethod.GET,entity ,String.class);
+        Listing newListing = listingBuilder
+                .reset()
+                .addName(listing.getName())
+                .addStock(listing.getStock())
+                .addId()
+                .addPrice(listing.getPrice())
+                .addSeller(listing.getSeller()) // masih belom bener
+                .build();
+
+        return newListing;
     }
 
 //    @Override
